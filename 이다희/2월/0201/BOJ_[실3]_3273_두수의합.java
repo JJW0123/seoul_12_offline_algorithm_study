cd7c3bc24f3726b734af5167750d478b60c966ec
Dahee <136606977+leedaaaaahee@users.noreply.github.com>
2026-02-01 13:13:43 +0900

    Create BOJ_[실3]_3273_두수의합.java
    [O(N²) → O(N)] 이중 루프를 투 포인터로 최적화하여 시간 초과 해결

---
+++ 이다희/2월/0201/BOJ_[실3]_3273_두수의합.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		
+		int x = Integer.parseInt(br.readLine());
+		
+		int start = 0;
+		int end = n - 1;
+		int answer = 0;
+		
+		while (start < end) {
+			if (arr[start] + arr[end] == x) answer++;
+			if (arr[start] + arr[end] < x) start++;
+			else end--;
+		}
+		
+		System.out.println(answer);
+		
+	}
+	
+}
