59421fff2bb0d9a5e3a1bd3f0b3fa01b41b8d64c
gudwns1812 <gudwns1812@naver.com>
2026-02-05 20:58:02 +0900

    refactoring code

--- 박형준/2월/0205/Boj_5430.kt
+++ 박형준/2월/0205/Boj_5430.kt
@@ -1,17 +1,17 @@
 fun main() {
     val T = readln().toInt()
-    for (tc in 1..T) {
-        val commands = readln().toCharArray()
+    repeat(T) {
+        val commands = readln()
         val n = readln().toInt()
 
         var rotate = true;
-        val core = readln().removeSurrounding("[", "]")
-        var list = ArrayDeque<Int>()
-        if (core.isNotEmpty()) {
-            val toList = core.split(",").map { it.toInt() }
-                .toList()
-
-            list = ArrayDeque(toList)
+        val core = readln()
+        val list = ArrayDeque<Int>()
+        if (n > 0) {
+            core.removeSurrounding("[", "]")
+                .split(",")
+                .map { it.toInt() }
+                .forEach { list.add(it) }
         }
 
         var success = true
@@ -19,20 +19,15 @@ fun main() {
             if (ch == 'R') {
                 rotate = !rotate
             } else if (ch == 'D') {
-                try {
-                    if (rotate) {
-                        list.removeFirst()
-                    } else {
-                        list.removeLast()
-                    }
-                } catch (e: RuntimeException) {
+                val nullCheck = if (rotate) list.removeFirstOrNull() else list.removeLastOrNull()
+                if (nullCheck == null) {
                     success = false
                     break
                 }
             }
         }
         if (success) {
-            val result = if (rotate) list.toList() else list.reversed()
+            val result = if (rotate) list.toList() else list.asReversed()
             println(result.joinToString(separator = ",", prefix = "[", postfix = "]"))
         } else {
             println("error")
