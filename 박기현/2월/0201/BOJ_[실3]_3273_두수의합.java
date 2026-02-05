872b42082b47a0a4f7b854ac175a2ccf1f4d6301
pkhjack2325 <pkhjack2325@gmail.com>
2026-02-05 17:47:08 +0900

    Implement solution for BOJ_3273 problem, including input handling and two-pointer technique for counting pairs.

---
+++ 박기현/2월/0201/BOJ_[실3]_3273_두수의합.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+public class BOJ_3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int x = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        int start = 0;
+        int end = n - 1;
+        int count = 0;
+        while (start < end) {
+            if (arr[start] + arr[end] == x) {
+                count++;
+                start++;
+                end--;
+            }
+            else if (arr[start] + arr[end] < x) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+        System.out.println(count);
+        br.close();
+    }
+}
