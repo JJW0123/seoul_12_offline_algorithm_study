1767563269267cf36da3b7ec896d6a4aae239b48
mphpX <hheennrryy5500@gmail.com>
2026-02-08 16:24:06 +0900

    solve SWEA_5215

---
+++ 박민혁/2월/0208_class/SWEA_5215.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public class SWEA_5215 {
+
+    static int N, L, Best;
+    static int[] Scores, Cals;
+    static void dfs(int idx, int scoreSum, int calSum){
+        if(calSum > L) return;
+        if(idx == N){
+            if(scoreSum > Best) Best = scoreSum;
+            return;
+        }
+        dfs(idx+1, scoreSum + Scores[idx], calSum + Cals[idx]);
+        dfs(idx+1, scoreSum, calSum);
+    }
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t;
+        t = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc<=t; tc++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+            Scores= new int[N];
+            Cals = new int[N];
+            for(int i = 0; i < N; i++){
+                st= new StringTokenizer(br.readLine());
+                Scores[i]= Integer.parseInt(st.nextToken());
+                Cals[i]= Integer.parseInt(st.nextToken());
+            }
+            Best = 0;
+            dfs(0, 0, 0);
+            System.out.printf("#%d %d\n",tc, Best);
+        }
+    }
+}
