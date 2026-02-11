82b10349a97a535b820d6f93b73c006c3f0c2e01
gudwns1812 <gudwns1812@naver.com>
2026-02-11 14:45:00 +0900

    BOJ 12865

---
+++ 박형준/2월/0211/BOJ_12865.kt
@@ -0,0 +1,14 @@
+import kotlin.math.max
+
+fun main() {
+    val (n,k) = readln().split(" ").map { it.toInt() }
+
+    val dp = IntArray(k + 1)
+    repeat(n) {
+        val (w , v) = readln().split(" ").map { it.toInt() }
+        for (i in k downTo w) {
+            dp[i] = max(dp[i], dp[i - w] + v)
+        }
+    }
+    println(dp[k])
+}
