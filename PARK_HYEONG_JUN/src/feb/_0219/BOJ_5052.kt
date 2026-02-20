9e0d377997be51c546ac3f058bf8e59cbe7f1761
gudwns1812 <gudwns1812@naver.com>
2026-02-20 11:53:10 +0900

    BOJ_5052

--- PARK_HYEONG_JUN/src/feb/_0219/BOJ_5052.kt
+++ PARK_HYEONG_JUN/src/feb/_0219/BOJ_5052.kt
@@ -2,6 +2,7 @@ package feb._0219
 
 fun main() {
     val tc = readln().toInt()
+    val sb = StringBuilder()
     repeat(tc) {
         val n = readln().toInt()
         val list = mutableListOf<String>()
@@ -9,15 +10,16 @@ fun main() {
         repeat(n) { list.add(readln()) }
         list.sortWith(compareBy<String> { it }.thenBy { it.length })
 
-        var result : String? = null
+        var result = true
         for (i in 0 until n - 1) {
             val prefix = list[i + 1].startsWith(list[i])
             if (prefix) {
-                result = "NO"
+                result = false
                 break
             }
         }
 
-        println(result ?: "YES")
+        sb.append(if (result) "YES" else "NO").append("\n")
     }
+    print(sb)
 }
